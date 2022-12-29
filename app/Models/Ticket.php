60a70d44bc32cc79a4f4f8b4c61dcf31494a3550
Ronildo Sousa <sousaronildo400@gmail.com>
2022-12-29 10:27:19 -0300

    fix(database): fix database structure, adding a file table

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
@@ -32,5 +33,10 @@ class Ticket extends Model
     public function labels(): BelongsToMany
     {
         return $this->belongsToMany(Label::class);
+    }
+
+    public function files(): HasMany
+    {
+        return $this->hasMany(File::class);
     }
 }
